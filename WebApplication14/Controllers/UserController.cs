0dab8b6fe6eca5d03bd7598ea6598704171db9bd
g5g1ta <129320277+g5g1ta@users.noreply.github.com>
2024-07-23 20:16:43 +0300

    Add files via upload

--- WebApplication14/Controllers/UserController.cs
+++ WebApplication14/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 using WebApplication14.Data;
 using WebApplication14.Models;
 
@@ -20,18 +21,8 @@ namespace WebApplication14.Controllers
 
         public async Task<IActionResult> GetInfo([FromRoute] int id)
         {
-            var email = HttpContext.Session.GetString("Email");
-            var uid = HttpContext.Session.GetInt32("UserId");
-            var password = HttpContext.Session.GetString("Password");
-            var role = HttpContext.Session.GetString("Role");
-            User user = new User()
-            {
-                Email = email,
-                UserId = id,
-                Password = password,
-                Role = role
-            };
-
+            
+            User user = GetCurrentUser();
 
             return View(user);
         }
@@ -39,11 +30,9 @@ namespace WebApplication14.Controllers
         [Route("users/getall")]
         public async Task<IActionResult> GetUsers()
         {
-            var email = HttpContext.Session.GetString("Email");
-            var uid = HttpContext.Session.GetInt32("UserId");
-            var password = HttpContext.Session.GetString("Password");
+            
             var role = HttpContext.Session.GetString("Role");
-            if(role == "Teacher")
+            if(role == "Admin")
             {
                 return View(await _context.users.ToListAsync());
             }
@@ -63,6 +52,149 @@ namespace WebApplication14.Controllers
                 .Where(x => x.classes.ClassesId == id)
                 .ToList();
             return View(studentsInClass);
+        }
+        [HttpGet]
+        [Route("users/addrole/{id}")]
+        public async Task<IActionResult> AddRole([FromRoute] int id)
+        {
+            var role = HttpContext.Session.GetString("Role");
+
+            var users = await _context.users.FirstOrDefaultAsync(x => x.UserId == id);
+            if(users != null && role == "Admin")
+            {
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpPost]
+        [Route("users/addrole/{id}")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> AddRole([FromRoute] int id, string role)
+        {
+            var users = await _context.users.FirstOrDefaultAsync(x => x.UserId == id);
+            if (users != null)
+            {
+                if (role == "Teacher")
+                {
+                    ChangeToTeacher(users);
+                }
+                else if (role == "Admin")
+                {
+                    ChangeToAdmin(users);
+                }
+                else if (role == "Student")
+                {
+                    ChangeToStudent(users);
+                }
+                
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public User GetCurrentUser()
+        {
+            var email = HttpContext.Session.GetString("Email");
+            var password = HttpContext.Session.GetString("Password");
+            var user = _context.users.FirstOrDefault(x => x.Email == email && x.Password == password);
+            if(user != null)
+            {
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void ChangeToTeacher(User user)
+        {
+            Teacher newTeacher = new Teacher();
+           
+            if(user.Role == "Student")
+            {
+                FindAndRemoveStudent(user);
+                MakeTeacher(user, newTeacher);                
+            }
+            else if(user.Role == "Admin")
+            {
+                FindAndRemoveAdmin(user);
+                MakeTeacher(user, newTeacher);
+            }          
+        }
+        public void ChangeToAdmin(User user)
+        {
+            Administrator newAdmin = new Administrator();
+            if(user.Role == "Student")
+            {
+                FindAndRemoveStudent(user);
+                MakeAdmin(user, newAdmin);
+            }
+            else if(user.Role == "Teacher")
+            {
+                FindAndRemoveTeacher(user);
+                MakeAdmin(user, newAdmin);
+            }
+        }
+        public void ChangeToStudent(User user)
+        {
+            Student newStudent = new Student();
+            if(user.Role == "Teacher")
+            {
+                FindAndRemoveTeacher(user);
+                MakeStudent(user, newStudent);
+            }
+            else if(user.Role == "Admin")
+            {
+                FindAndRemoveAdmin(user);
+                MakeStudent(user, newStudent);
+            }
+        }
+        public void FindAndRemoveStudent(User user)
+        {
+            var FindStudent = _context.student.FirstOrDefault(x => x.UserId == user.UserId);
+            if (FindStudent != null)
+            {
+                _context.student.Remove(FindStudent);
+                _context.SaveChanges();
+            }
+        }
+        public void FindAndRemoveAdmin(User user)
+        {
+            var FindAdmin = _context.administrators.FirstOrDefault(x => x.UserId == user.UserId);
+            if(FindAdmin != null)
+            {
+                _context.administrators.Remove(FindAdmin);
+                _context.SaveChanges();
+            }
+        }
+        public void FindAndRemoveTeacher(User user)
+        {
+            var FindTeacher = _context.teachers.FirstOrDefault(x => x.UserId == user.UserId);
+            if(FindTeacher != null)
+            {
+                _context.teachers.Remove(FindTeacher);
+                _context.SaveChanges();
+            }
+        }
+        public void MakeTeacher(User user, Teacher teacher)
+        {
+            user.Role = "Teacher";
+            teacher.UserId= user.UserId;
+            _context.teachers.Add(teacher);
+            _context.SaveChanges();
+        }
+        public void MakeAdmin(User user, Administrator admin)
+        {
+            user.Role = "Admin";
+            admin.UserId= user.UserId;
+            _context.administrators.Add(admin);
+            _context.SaveChanges();
+        }
+        public void MakeStudent(User user, Student student)
+        {
+            user.Role = "Student";
+            student.UserId= user.UserId;
+            _context.student.Add(student);
+            _context.SaveChanges();
         }
     }
 }
